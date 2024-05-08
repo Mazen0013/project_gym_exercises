f5b5223c6c77a901767be3f3c2278dd5b200c680
mazenais <mazen.aboismail@gmail.com>
2024-05-08 04:51:19 +0200

    adding nav and logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
 
 const Home = () => {
     return (
-        <div>
+        <Box>
+
             
-        </div>
+        </Box>
     )
 }
 
