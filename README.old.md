869899402cfa6f9361ca8561fe0d1b96c5f2b02f
mazenais <mazen.aboismail@gmail.com>
2024-05-08 04:48:04 +0200

    adding nav bar and logo

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# project_gym
+A project of a gym app with exercises to choose from and add to your routine.
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+// using material ui, the box is essentially just a div with some shading and colors //
+import { Box } from "@mui/material";
+
+import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    //adding sx property for making sure it's responsive on different devices, adding margin auto//
+    // material ui allows us to do that on the Box component//
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercises/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
